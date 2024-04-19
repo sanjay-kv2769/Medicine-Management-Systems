41aa5f7a435a61dc276b32836863f1c9bbb5258b
Sanjay K V <93617720+sanjay-kv2769@users.noreply.github.com>
2024-04-19 22:11:08 +0530

    Update medicineSchema.js

--- src/models/medicineSchema.js
+++ src/models/medicineSchema.js
@@ -9,6 +9,7 @@ const medicineSchema = new mongoose.Schema({
   quantity: { type: String, required: true },
   stock: { type: Number, required: true, default: 1 },
   price: { type: Number, required: true },
+  expiry_date:{type: String, required: true},
   image: { type: String, required: true },
 });
 
